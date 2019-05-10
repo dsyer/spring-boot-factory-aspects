6a75e4c85d993496302cc14a57db28ccae88e40a
Dave Syer <dsyer@pivotal.io>
2019-05-10 13:50:23 +0100

    An AspectJ project to inject behaviour into Boot

---
+++ src/main/java/com/example/FactoryInterceptor.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.util.Properties;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationListener;
+
+@Aspect
+public class FactoryInterceptor {
+
+	private static final String[] EXCLUDES = { ApplicationListener.class.getName(),
+			ApplicationContextInitializer.class.getName() };
+
+	@Around("execution(java.util.Properties org.springframework.core.io.support.PropertiesLoaderUtils.loadProperties(..))")
+	public Object stack(ProceedingJoinPoint joinPoint) throws Throwable {
+		Properties proceed = (Properties) joinPoint.proceed();
+		Properties result = new Properties();
+		result.putAll(proceed);
+		for (String name : EXCLUDES) {
+			if (proceed.containsKey(name)) {
+				result.remove(name);
+			}
+		}
+		return result;
+	}
+
+}
